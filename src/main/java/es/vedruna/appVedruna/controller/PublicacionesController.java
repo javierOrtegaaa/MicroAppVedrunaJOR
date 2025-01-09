a2a0723ebd4138ca6cb7937446fc0ff03f0a241b
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2025-01-09 14:12:36 +0100

    feat: añadir swagger

--- src/main/java/es/vedruna/appVedruna/controller/PublicacionesController.java
+++ src/main/java/es/vedruna/appVedruna/controller/PublicacionesController.java
@@ -3,6 +3,7 @@ package es.vedruna.appVedruna.controller;
 
 import es.vedruna.appVedruna.model.Publicacion;
 import es.vedruna.appVedruna.services.PublicacionServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,18 +15,26 @@ import java.util.List;
 @AllArgsConstructor
 public class PublicacionesController {
     private final PublicacionServiceImpl publicacionServiceImpl;
-
-    // Crear un nuevo usuario
+    
+    @Operation(
+            summary = "Crear una nueva publicación"
+    )
     @PostMapping()
     public Publicacion createPublicacion(@RequestBody Publicacion publicacion) {
         return publicacionServiceImpl.createPublicacion(publicacion);
     }
-    
+
+    @Operation(
+            summary = "Añadir like"
+    )
     @PutMapping("/put/{id}/{id_user}")
     public Publicacion updateLike(@PathVariable String id, @PathVariable String id_user){
         return publicacionServiceImpl.updateLike(id, id_user);
     }
-    // Obtener todos los usuarios
+
+    @Operation(
+            summary = "Obtener todos los usuarios"
+    )
     @GetMapping()
     public List<Publicacion> getAllPublicaciones() {
         return publicacionServiceImpl.getAllPublicaciones();
